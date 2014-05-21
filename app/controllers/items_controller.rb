d470dc7621303f1f327fc27a1b19f77cbf92a896
RyanSnodgrass <res0428@yahoo.com>
2014-05-21 10:31:17 -0400

    etsy grabbing with name and description but with redundant

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,7 @@ class ItemsController < ApplicationController
 	def create
 		@item = Item.new(item_params)
 		if @item.save
+
 			redirect_to wishlist_path(params[:wishlist_id]), notice: "Item added."
 		else
 			redirect_to :back, alert: "Failed to save."
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,5 +10,21 @@ class Item < ActiveRecord::Base
 		self[:etsy_id] = numbers[0]
 		self.save #save in the DB
 	end
+
+	def set_name_from_etsy
+		etsy_data = get_etsy_data
+		self[:name] = etsy_data["results"][0]["title"]
+		self.save
+	end
+
+	def set_description_from_etsy
+		etsy_data = get_etsy_data
+		self[:description] = etsy_data["results"][0]["description"]
+		self.save
+	end
+
+	def get_etsy_data
+		response = HTTParty.get("https://openapi.etsy.com/v2/listings/#{self.etsy_id}?api_key=#{Rails.application.secrets.etsy_api_key}")
+	end
 end
 
