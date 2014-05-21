4eaaae39b3d72abe11f14ffef9ca96d01c85f831
RyanSnodgrass <res0428@yahoo.com>
2014-05-21 09:59:35 -0400

    added getting etsy_id and url

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ActiveRecord::Base
 	belongs_to :wishlist
+
+	after_create :get_etsy_id
+	
+	def get_etsy_id
+		string = self.etsy_url
+		regex = /\d+/
+		numbers = string.scan(regex)
+		self[:etsy_id] = numbers[0]
+		self.save #save in the DB
+	end
 end
+
